26d4e49ad308ec0544d8a1673b0f1a0ff52e768e
Serhii Koliadiuk <serhii.koliadiuk@IMPRIVATA.com>
2023-09-20 17:14:28 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { connectToMongoDB } = require('./db');
+const urlRoute = require('./routes/index');
+const URL = require('./models/shortUrl.model');
+
+const app = express();
+const PORT = 5000;
+
+connectToMongoDB('mongodb://localhost:27017/short-url').then(() => console.log('Mongodb connected'));
+
+app.use(express.json());
+
+app.use('/url', urlRoute);
+
+app.get('/:shortId', async (req, res) => {
+	const shortId = req.params.shortId;
+	const entry = await URL.findOneAndUpdate(
+		{
+			shortId,
+		},
+		{
+			$push: {
+				visitHistory: {
+					timestamp: Date.now(),
+				},
+			},
+		}
+	);
+	res.redirect(entry.redirectURL);
+});
+
+app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
